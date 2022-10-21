ed40e6843342b6cf76470c3d65ada8b5baa59bf3
Alejandra Cabello <alejandracabellomunoz@gmail.com>
2022-10-21 11:30:11 +0200

    ejercicio7

--- js/checkout.js
+++ js/checkout.js
@@ -1,28 +1,127 @@
 
 // Exercise 6
+let error = 0;
 function validate() {
-	var error = 0;
-	// Get the input fields
-	var fName = document.getElementById("fName");
-	var fEmail = document.getElementById("fEmail");
+	let fName = document.getElementById("fName");
+	let fEmail = document.getElementById("fEmail");
+	let fLastName = document.getElementById("fLastN");
+	let fAdress = document.getElementById("fAddress");
+	let fPhone = document.getElementById("fPhone");
+	let fPassword = document.getElementById("fPassword");
 
-	// Get the error elements
+	// Get the error elements. del div en donde se indica que el campo no es válido
+	//cuando este bien cambiaré la clase y el mensaje-*/
 	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");  
-	
-	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
-		error++;
+	var errorEmail = document.getElementById("errorEmail");
+	let errorLastName = document.getElementById("errorLastN");
+	let errorAdress = document.getElementById("errorAddress");
+	let errorPhone = document.getElementById("errorPhone");
+	let errorPassword = document.getElementById("errorPassword");
+
+
+	let expReg = {
+		name: /^[a-zA-ZÀ-ÿ\s]{3,40}$/,
+		email: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,//no consigo que me salga esta expReg
+		adress: /^[a-zA-ZÀ-ÿ\s]{3,40}\s+\d+/,
+		lastName: /^[a-zA-ZÀ-ÿ\s]{3,40}$/,
+		password: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
+		phone: /^(\d\s?){9,11}$/,
+	}
+	//validacion del nombre 
+	let nombreValido = expReg.name.test(fName.value);
+	console.log(fName.value);
+
+	if (nombreValido == false) {//el nombre no es valido
+		fName.style.borderColor = "red";
+		errorName.style.display = "block";
+		error += 1;
+	}
+	else {
+		fName.style.borderColor = "green";
+		errorName.style.display = "none";
+			}
+
+	//validacion del apellido
+	console.log(fLastN.value);
+	let apellidoValido = expReg.lastName.test(fLastN.value);
+	if (apellidoValido == false) {//el nombre no es válid
+		fLastName.style.borderColor = "red";
+		errorLastName.style.display = "block";
+		error += 1;
+	}
+	else {
+		fLastName.style.borderColor = "green";
+		errorLastName.style.display = "none";
+			}
+
+
+	//validacion del email 
+	console.log(fEmail.value);
+	let emailValido = expReg.name.test(fEmail.value);
+	if (emailValido == false) {//el nombre no es válid
+		fEmail.style.borderColor = "red";
+		errorEmail.style.display = "block";
+		error += 1;
+	}
+	else {
+		fEmail.style.borderColor = "green";
+		errorEmail.style.display = "none";
 	}
 
-	if(fEmail.value == ""){
-		error++;
+	//validacion del password 
+	console.log(fPassword.value);
+	let passwordValido = expReg.name.test(fPassword.value);
+	if (passwordValido == false) {//el nombre no es válid
+		fPassword.style.borderColor = "red";
+		errorPassword.style.display = "block";
+		error += 1;
 	}
-	 
-	if(error>0){
-		alert("Error");
-	}else{
-		alert("OK");
+	else {
+		fPassword.style.borderColor = "green";
+		errorPassword.style.display = "none";
 	}
 
+	//validacion del nombre 
+	console.log(fPhone.value);
+	let phoneValido = expReg.name.test(fName.value);
+	if (phoneValido == false) {//el nombre no es válid
+		fPhone.style.borderColor = "red";
+		errorPhone.style.display = "block";
+		error += 1;
+	}
+	else {
+		fPhone.style.borderColor = "green";
+		errorPhone.style.display = "none";
+	}
+
+	//validacion del nombre 
+	console.log(fAdress.value);
+	let adressValido = expReg.name.test(fName.value);
+	if (adressValido == false) {//el nombre no es válid
+		fAdress.style.borderColor = "red";
+		errorAdress.style.display = "block";
+		error += 1;
+	}
+	else {
+		fAdress.style.borderColor = "green";
+		errorAdress.style.display = "none";
+	}
+	formSaver();
+
+}
+function formSaver() {
+	if (error > 0) {
+		alert("Error, all the fields might be filled");
+	} else {
+		alert("OK, correct submited");
+		fName.value = "";
+		fLastName.value = "";
+		fAdress.value = "";
+		fPhone.value = 0;
+		fEmail.value = "";
+		fPassword = "";
+		document.querySelectorAll("valid-feedback").display = "none";
+		document.querySelectorAll("invalid-feedback").display = "none";
+
+	}
 }
